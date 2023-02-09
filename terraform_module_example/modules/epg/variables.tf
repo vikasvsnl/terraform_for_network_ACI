666b650d0fe02583d9c6e5e2a57c44b095dbe256
vikasvsnl <59799662+vikasvsnl@users.noreply.github.com>
2023-02-09 12:54:44 +0530

    Create variables.tf

---
+++ terraform_module_example/modules/epg/variables.tf
@@ -0,0 +1,15 @@
+variable "epg" {
+    default = ""
+    }
+variable "bridge_output" {
+    default = ""
+    }
+variable "application_output" {
+    default = ""
+    }
+variable "app_epg" {
+   default = "app_epg"
+}
+variable "tenant_output" {
+    default = ""
+    }
