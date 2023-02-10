a9916a69323d5bdd325c44ddf8a0f1e529b7f196
vikasvsnl <59799662+vikasvsnl@users.noreply.github.com>
2023-02-10 14:21:44 +0530

    Create variables.tf

---
+++ terraform_module_example/modules/Leaf_profile/variables.tf
@@ -0,0 +1,14 @@
+variable "leaf_profile_name" {
+    default = ""
+    }
+
+variable "leaf_block" {
+    default = ""
+    }
+
+variable "leaf_name" {
+    default = ""
+    }
+variable "leaf_number" {
+    default = ""
+    }
