4c480517a7d4182c49bc5d62574d539259955deb
vikasvsnl <59799662+vikasvsnl@users.noreply.github.com>
2023-02-14 12:24:18 +0530

    Create locals.tf

---
+++ Loop_example/locals.tf
@@ -0,0 +1,33 @@
+locals {
+
+ vrfs = ["RED", "BLUE"]
+
+ bds = [
+
+    {
+
+      "vrf_name": "RED",
+
+      "bd_name": "BD500"
+
+    },
+
+    {
+
+      "vrf_name": "BLUE",
+
+      "bd_name": "BD501"
+
+    },
+
+    {
+
+      "vrf_name": "BLUE",
+
+      "bd_name": "BD502"
+
+    }
+
+  ]
+
+}
